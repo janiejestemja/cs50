4e5e1528b2edc6a1159bf050c6897be4a55b1cda
janiejestemja <janiejestemja@web.de>
2024-10-16 15:43:03 +0200

    Added inline comments.

--- P/1pset/bank/bank.py
+++ P/1pset/bank/bank.py
@@ -1,11 +1,18 @@
 # Home Federal Savings Bank
 
 def main():
+	# asking user for input and calling .lower() method
 	given_greeting = input("Greeting: ").lower()
+
+	# checking for beginning with "hello"
 	if given_greeting.startswith("hello"):
 		print("0$")
+	
+	# checking for beginning with "h"
 	elif given_greeting.startswith("h"):
 		print("20$")
+
+	# printing "100$" otherwise
 	else:
 		print("100$")
 
--- P/1pset/deep/deep.py
+++ P/1pset/deep/deep.py
@@ -1,10 +1,15 @@
 # Deep Thought
 
 def main():
+	# asking user for input and forcing it to lowercase
 	user_input = input("What is the Answer to the Great Qestion of Life, the Universe, and Everything?: ").lower()
+	# cecking disjunction of conditions 
 	if user_input == "42" or user_input == "forty two" or user_input == "forty-two":
+		# printing yes if a condition is met
 		print("Yes")
+
 	else:
+		# printing no otherwise
 		print("No")
 
 if __name__ == "__main__":
