17be2e51dcb8385c618267c1ed846be2bbc4e81d
janiejestemja <janiejestemja@web.de>
2024-10-17 01:58:09 +0200

    Added inline comments and docstring.

--- P/3pset/taqueria/taqueria.py
+++ P/3pset/taqueria/taqueria.py
@@ -1,22 +1,39 @@
 # Felipes Taqueria
 
 def main():
+	# initialising total with zero.
 	total = 0
 
+	# setting up infinite loop for multiline input
 	while True:
+		# trying to ask user for input
 		try:
 			item = input("Item: ").title()
 
+		# handling EOFError by printing a new line and breaking the loop
 		except EOFError:
 			print("")
 			break
 
 		else:
+			# calculating new total by calling helper_func
 			total = total + helper_func(item)
+
+			# printing the current total
 			print(f"Total: {total}")
 
 def helper_func(item):
+	"""
+	Checks if an item is on the given menu and returns the price.
 
+	Parameters:
+	item (str): A potential name of an item on the given menu.
+
+	Returns:
+	float: Price of the item if on the menu otherwise zero.
+	"""
+
+	# definition of the menu as a dict.
 	menu = {
     "Baja Taco": 4.25,
     "Burrito": 7.50,
@@ -29,10 +46,12 @@ def helper_func(item):
     "Tortilla Salad": 8.00
 }
 
+	# returning price associated with the item if it is on the menu
 	if item in menu:
 		return menu[item]
 
 	else:
+		# otherwise returning zero
 		return 0
 
 
