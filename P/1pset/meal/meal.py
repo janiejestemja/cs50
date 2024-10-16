a3eb85d27ba46ee74499f9c1ca762f43bc5a520f
janiejestemja <janiejestemja@web.de>
2024-10-16 16:44:54 +0200

    Added inline commments and docstring.

--- P/1pset/meal/meal.py
+++ P/1pset/meal/meal.py
@@ -1,24 +1,39 @@
 # Meal Time
 
 def main():
+	# asking for user input, stripping whitespace, calling convert()
 	number_of_hours = convert(input("Time:").strip())
 
+	# checking for breakfast time between 7 and 8 o'clock
 	if 8 >= number_of_hours >= 7: 
 		print("breakfast time")
 
+	# checking for breakfast time between 12 and 13 o'clock
 	elif 13 >= number_of_hours >= 12:
 		print("lunch time")
 
+	# checking for breakfast time between 18 and 19 o'clock
 	elif 19 >= number_of_hours >= 18:
 		print("dimmer time")
 
-	else:
-		return 0
 
 
 def convert(time):
+	"""
+	Converts a str containing a time to a floating point value.
+
+	Parameters:
+	time (str): a str containing a time formatted like "%H:%M".
+
+	Returns:
+	float: a floating point value representing the given time
+	as floating point number of hours.
+	"""
+
+	# splitting time into hours and minutes
 	hours, minutes = time.split(":")
 
+	# return calculated floating point reprepresentation of given time
 	return float(int(hours) + int(minutes) / 60)
 
 
