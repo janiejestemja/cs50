436ba50bbf35b580f1a1c88f5d695f3a6459d082
janiejestemja <janiejestemja@web.de>
2024-10-17 02:32:22 +0200

    Added inline comments.

--- P/4pset/game/game.py
+++ P/4pset/game/game.py
@@ -3,33 +3,45 @@
 from random import randint
 
 def main():
+    # infinite loop to reprompt the user
     while True:
 
+        # trying to ask user for input and typeforcing the input to int
         try:
             level = int(input("Level: "))
 
+        # passing by ValueError to repromt user
         except ValueError:
             pass
 
         else:
+            # otherweise generating a random number between one and the users input
             solution = randint(1, level)
+
+            # exiting the input loop
             break
 
+    # gameloop
     while True:
 
+        # trying to typeforce users input 
         try:
             guess = int(input("Guess: "))
         
+        # handling ValueError to reprompt user if typeforcing failed
         except ValueError:
             pass
         
         else:
+            # otherwise giving the user hints
             if guess > solution:
                 print("Too small!")
 
             elif guess < solution:
                 print("Too large!")
 
+            # in the case of the user guessed the randomly generated number 
+            # printing the success and exiting the gameloop
             else:
                 print("Just right!")
                 break
