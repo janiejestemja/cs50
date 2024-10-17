11f3d189199c108bf6912da6c9556c10a4497e06
janiejestemja <janiejestemja@web.de>
2024-10-17 02:06:04 +0200

    Added inline comments.

--- P/4pset/adieu/adieu.py
+++ P/4pset/adieu/adieu.py
@@ -3,21 +3,33 @@
 from inflect import engine 
 
 def main():
+    # assigning variable p per inflect documentation
     p = engine()
+
+    # defining empty list 
     names = []
 
+    # starting infinte loop for multiline input
     while True:
+        
+        # trying to get user input
         try:
             name = input("Name: ")
 
+        # handling EOFError
         except EOFError:
+            # assign formatted_names variable per inflect documentation
             formatted_names = p.join(names, final_sep="")
 
+            # printing new line and the formatted names
             print("")
             print("Adieu, adieu, to", formatted_names)
+            
+            # breaking the input loop
             break
 
         else:
+            # appending the line inputted by the user to the list
             names.append(name)
 
 
