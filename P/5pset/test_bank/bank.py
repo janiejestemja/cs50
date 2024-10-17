63ec70b904bee3ffb5dc3513f7192eb8e159b104
janiejestemja <janiejestemja@web.de>
2024-10-17 20:55:06 +0200

    Added inline comments and a docstring.

--- P/5pset/test_bank/bank.py
+++ P/5pset/test_bank/bank.py
@@ -1,15 +1,33 @@
 # Home Federal Savings Bank
 
 def main():
+	# asking user for input
 	given_greeting = input("Greeting: ")
+	# passing users input to value function and printing out 
+	# formatted result
 	print(f"{value(given_greeting)}$")
 
 def value(greeting):
-	
+	"""
+	Assigns a value to a string depending on what is starts with. 
+
+	Parameters:
+	greeting (str): A potential greeting.
+
+	Returns:
+	int: 0, 20, 100.
+
+	Raises:
+	ValueError: In case the argument is not a str.
+	"""
+	# checking type of argument and raising exception if not a str
 	if type(greeting) != type(str()):
 		raise ValueError("expecting a str()")
 	
+	# forcing argument to lowercase
 	greeting = greeting.lower()
+
+	# checking start of string and returning the respectively assigned value
 	if greeting.startswith("hello"):
 		return 0
 	elif greeting.startswith("h"):
--- P/5pset/test_bank/test_bank.py
+++ P/5pset/test_bank/test_bank.py
@@ -2,18 +2,21 @@
 
 from bank import value
 
+# testing variations of the word "hello"
 def test_hello():
     assert value("Hello") == 0
     assert value("hello") == 0
     assert value("hEllO") == 0
     assert value("Hello, dear ...") == 0
 
+# testing variations of greetings starting with "h"
 def test_h():
     assert value("Hej") == 20
     assert value("hej") == 20
     assert value("Hi") == 20
     assert value("Hej, dear customer") == 20
 
+# testing variations of greetings not starting with "h"
 def test_other_greetings():
     assert value("Welcome") == 100
     assert value("welcome") == 100
