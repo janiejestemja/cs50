fe504ffa05a11e507e8764992cbcf7fbad42f528
janiejestemja <janiejestemja@web.de>
2024-10-17 02:21:46 +0200

    Added inline comments.

--- P/4pset/emojize/emojize.py
+++ P/4pset/emojize/emojize.py
@@ -3,6 +3,7 @@
 import emoji
 
 def main():
+	# using emoji per emoji documentation passing user input directly as an argument
 	print("Output:", emoji.emojize(input("Input: ")))
 
 if __name__ == "__main__":
--- P/4pset/figlet/figlet.py
+++ P/4pset/figlet/figlet.py
@@ -5,27 +5,43 @@ from random import choice
 from pyfiglet import Figlet
 
 def main():
+	# assinging figlet variable per instructions in hints-section
 	figlet = Figlet()
 
+	# checking for invalid number of command line arguments (cla)
 	if len(sys.argv) != 1 and len(sys.argv) != 3:
 		sys.exit("Error: Unexpected number of commandlinearguments (expects none or two)")
 	
+	# checking for valid number of cla's
 	if len(sys.argv) == 1:
+		# picking a random font
 		font = choice(figlet.getFonts())
 
+	# checking for valid number of cla's 
 	if len(sys.argv) == 3:
+		# checking for first cla being neither "-f" nor "--font"
 		if sys.argv[1] != "-f" and sys.argv[1] != "--font":
+
+			# exiting via sys.exit
 			sys.exit("Error: first commandlineargument should be either '-f' or '--font'.")
 
+		# checking for second cla not being in fonts provided by pyfiglet
 		elif sys.argv[2] not in figlet.getFonts():
+
+			# exiting via sys.exit
 			sys.exit("Error: chosen font not found.")
 
 		else:
+			# otherwise defining font as defined in cla
 			font = sys.argv[2]
 
+	# asking user for input
 	user_input = input("Input: ")
 	
+	# setting font per instructions in hint section
 	figlet.setFont(font=font)
+	
+	# printing users input formatted per pyfiglet
 	print(figlet.renderText(user_input))
 
 if __name__ == "__main__":
