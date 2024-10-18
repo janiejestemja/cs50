474e1a40d6335a8de016514edbb1233420b9bc91
janiejestemja <janiejestemja@web.de>
2024-10-18 18:59:29 +0200

    Added inline comments and docstrings.

--- P/5pset/test_fuel/fuel.py
+++ P/5pset/test_fuel/fuel.py
@@ -1,42 +1,80 @@
 # Fuel Gauge
 
 def main():
+	# setting up infinite loop
 	while True:
+		# asking for input
 		fraction = input("Fraction: ").strip()
 
 		try:
+			# trying conversion 
 			percentage = convert(fraction)
+
+		# passing by exceptions
 		except (ValueError, ZeroDivisionError):
 			pass
+		# priting result
 		else:
 			print(gauge(percentage))
 
 def convert(fraction):
+	"""
+	Converts a fraction to a number.
+
+	Parameters:
+	fraction (str): A str in X/Y format where X an Y are integers.
+
+	Returns:
+	int: Nearest int between 0 and 100, inclusive.
+
+	Raises:
+	ValueError: If X is greater than Y.
+	ZeroDivisionError: If Y is zero.
+	"""
+	# splitting input into nominator and denominator
 	nominator, denominator = fraction.split("/")
+
+	# trying typoforcing input
 	try:
 		nominator = int(nominator)
 		denominator = int(denominator)
+	# handling exception
 	except ValueError:
 		raise ValueError("given variables are not recognized as integers.")
+
 	else:
+		# trying division
 		try:
 			result = nominator / denominator
 
+		# handling exception
 		except ZeroDivisionError:
 			raise ZeroDivisionError("we do not divide through zero.")
 
 		else:
+			# multiplying number by hundret, rounding to nearest int and returning result
 			result = result * 100
 			return round(result)
 
 def gauge(percentage):
+	"""
+	Represents a fuel-gauge.
+
+	Parameters:
+	percentage (int/float): A number.
+
+	Returns:
+	str: A str representing the output of a fuel-gauge.
+	"""
     
+	# chcking for fullness and emptyness
 	if percentage > 99:
 		return "F"
 
 	elif percentage < 1:
 		return "E"
 
+	# if neither full nor empty printing percentage per formatstring.
 	else:
 		return f"{percentage}%"
 
--- P/5pset/test_fuel/test_fuel.py
+++ P/5pset/test_fuel/test_fuel.py
@@ -4,11 +4,13 @@ import pytest
 from fuel import convert, gauge
 
 def test_convert():
+    # testing exceptions
     with pytest.raises(ZeroDivisionError):
         convert("1/0")
     with pytest.raises(ValueError):
         convert("cat/dog")
 
+    # testing assertions
     assert convert("0/100") == 0
     assert convert("25/100") == 25
     assert convert("50/100") == 50
@@ -16,8 +18,10 @@ def test_convert():
     assert convert("100/100") == 100
 
 def test_gauge():
+    # looping through general cases
     for i in range(1, 100):
         assert gauge(i) == f"{i}%"
 
+    # testing cornvercases
     assert gauge(0) == "E"
     assert gauge(100) == "F"
